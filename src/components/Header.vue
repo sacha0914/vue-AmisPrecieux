334526c3c90f6ef272852af1b9ca543b037245c3
sacha0914 <sacha0914.poidevin@gmail.com>
2023-05-09 22:39:29 +0200

    Nav and Home

---
+++ src/components/Header.vue
@@ -0,0 +1,111 @@
+<script setup>
+import { ref } from 'vue';
+
+const state = ref(false);
+
+
+
+</script>
+
+<template>
+<div id="Header">
+    <div class="logo">
+        <img class="logoPanda" src="../assets/LogoMyDigitalStartUp 3.svg" alt="">
+    </div>
+    <!-- menu mobile -->
+    <div class="burger" @click="state = !state" :class="{'burger-open' : state}" >
+        <div class="trait"></div>
+        <div class="trait"></div>
+        <div class="trait"></div>
+    </div>
+    <div class="menu--burger" :class="{'open' : state}"  >
+    
+    </div>
+    <!-- menu mobile -->
+
+    <!-- menu dekstop -->
+    <div class="nav--icon">
+    <font-awesome-icon class="Icon" icon="fa-solid fa-cart-shopping" size="2xl" style="color: #000000;" />
+
+    <font-awesome-icon class="Icon" icon="fa-solid fa-user" size="2xl" style="color: #000000;"/> 
+    </div>  
+    <!-- <ul class="links">
+        <li v-for="(link, index) in links" :key="index">{{ link.name }}</li>
+    </ul> -->
+    <!-- menu dekstop -->
+
+
+
+</div>
+</template>
+
+<style scoped>
+#Header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #D2EDD3;
+    padding: 20px;
+}
+#Header .logoPanda{
+    height: 15vh;
+    padding-left: 150px;
+}
+#Header .Icon{
+height: 5vh;
+padding-right: 400px;
+
+
+}
+#Header .burger .trait{
+    height: 3px;
+    width: 23px;
+    background: #000;
+    margin-bottom: 3px;
+}
+#Header .nav--icon{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 100px;
+}
+#Header .menu--burger{
+    height: 100vh;
+    width: 100%;
+    left: 100%;
+    background: #D2EDD3;
+    position:fixed ;
+    transition: 0.3s;
+}
+#Header .open{
+    left: 0%;
+}
+#Header .burger-open{
+    z-index: 2;
+    background: #D2EDD3;
+ }
+#Header .links{
+    display: none;
+}
+
+
+
+@media screen and (min-width: 700px){
+   #Header .burger 
+    ,#Header .menu--burger {
+        display: none;
+    }
+   
+}
+@media screen and (max-width: 700px){
+   #Header .Icon{
+    display: none;
+   }
+    #Header .logoPanda{
+        display: none;
+    }
+    #Header  .nav--icon{
+        display: none;
+    }
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <div id="Home">
+        <div class="AmisPrecieux">
+            <div class="Discover">
+                <h1 class="AmisTitle">Amis Précieux </h1>
+            </div>
+            <div class="Logo">
+                <img class="PointCercle" src="../assets/Hero Image Spot.svg" alt="">
+                <img class="Panda" src="../assets/panda.svg" alt="">
+            </div>
+        </div>
+        <div class="Separation">
+        <img class="Line" src="../assets/Line-Dash.svg" alt="">
+    </div>
+    </div>
+</template>
+
+<style scoped>
+#Home {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding-top: 10%;
+    margin: 100px;
+}
+
+#Home .Logo {
+    display: flex;
+
+}
+
+#Home .PointCercle {
+    width: 40vh;
+    z-index: 1;
+
+}
+
+#Home .Panda {
+    padding-left: 3%;
+    width: 40vh;
+    z-index: 2;
+    position: absolute;
+
+}
+
+
+
+#Home .Discover {
+    font-size: 130px;
+    margin: 50px;
+}
+
+#Home .AmisPrecieux {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 250px;
+}
+#Home .Separation {
+    /* padding: 300px; */
+    width: 100vh;
+    background-repeat: repeat-x;
+}
+#Home .Line {
+    width: 100vh;
+    padding-top: 25%;
+background-repeat: repeat;
+
+}
+
+
+@media screen and (max-width: 700px) {
+    #Header .Icon {
+        display: none;
+    }
+
+    #Header .logoPanda {
+        display: none;
+    }
+
+    #Header .nav--icon {
+        display: none;
+    }
+}</style>
