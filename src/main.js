8fca2baf318f4fe8fd7e2f00c211c44e34d11148
SACHA POIDEVIN <96061674+sacha0914@users.noreply.github.com>
2023-09-29 12:55:11 +0200

    Change directory (#3)

--- src/main.js
+++ src/main.js
@@ -11,8 +11,21 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faShoppingCart, faUser, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
+
+  
 /* add icons to the library */
 library.add(faUserSecret, faShoppingCart, faUser,faInstagram)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      // Définissez vos routes ici
+    ],
+  });
+
+  createApp(App)
+  .use(router) // Utilisez le routeur
+  .mount('#app');
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
